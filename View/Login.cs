1598bc98d79771c78720da9239cfa4b131f4f918
damara <damarmuhamad13@gmail.com>
2023-12-23 17:35:09 +0700

    feat: add base view login

---
+++ View/Login.cs
@@ -0,0 +1,13 @@
+﻿using ParkEase.Constants;
+
+namespace ParkEase.View
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+            this.Text = $"{App.APP_NAME} - Login";
+        }
+    }
+}
