3fcc3de9f8c35de422b21ecb7890e63a633247e9
damara <damarmuhamad13@gmail.com>
2023-12-26 16:36:49 +0700

    feat: add winform entrance ticket

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -323,6 +323,16 @@ namespace ParkEase.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        public static System.Drawing.Bitmap picture_changepassword {
+            get {
+                object obj = ResourceManager.GetObject("picture_changepassword", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         public static System.Drawing.Bitmap picture_entry {
             get {
                 object obj = ResourceManager.GetObject("picture_entry", resourceCulture);
