a8b240f7597769eb46b554cadbeb779c6e9699f1
Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2023-12-27 02:06:06 +0700

    refactor: dashboard sidebar positoning

--- View/Dashboard.cs
+++ View/Dashboard.cs
@@ -27,25 +27,9 @@ namespace ParkEase.View
             imgSlide.SendToBack();
         }
 
-        private void guna2Button1_CheckedChanged(object sender, EventArgs e)
+        private void btnDashboard_CheckedChanged(object sender, EventArgs e)
         {
             MoveImageBox(sender);
-        }
-
-        public const int WM_NCLBUTTONDOWN = 0xA1;
-        public const int HTCAPTION = 0x2;
-        [DllImport("User32.dll")]
-        public static extern bool ReleaseCapture();
-        [DllImport("User32.dll")]
-        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
-
-        private void OnMouseDown(object sender, MouseEventArgs e)
-        {
-            if (e.Button == MouseButtons.Left)
-            {
-                ReleaseCapture();
-                SendMessage(Handle, WM_NCLBUTTONDOWN, HTCAPTION, 0);
-            }
         }
     }
 }
