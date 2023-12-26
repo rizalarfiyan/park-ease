4533f50b65fec7ab2fe1dd38c275911b7a557c47
damara <damarmuhamad13@gmail.com>
2023-12-26 08:21:55 +0700

    feat: add change password page

--- View/ChangePassword.Designer.cs
+++ View/ChangePassword.Designer.cs
@@ -29,11 +29,287 @@
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            uC_TopBar1 = new UserControls.UC_TopBar();
+            guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(components);
+            guna2CustomGradientPanel1 = new Guna.UI2.WinForms.Guna2CustomGradientPanel();
+            linkLabel1 = new LinkLabel();
+            pictureBox1 = new PictureBox();
+            guna2TextBox1 = new Guna.UI2.WinForms.Guna2TextBox();
+            guna2TextBox2 = new Guna.UI2.WinForms.Guna2TextBox();
+            guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            guna2Elipse2 = new Guna.UI2.WinForms.Guna2Elipse(components);
+            ((System.ComponentModel.ISupportInitialize)guna2PictureBox1).BeginInit();
+            guna2CustomGradientPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // uC_TopBar1
+            // 
+            uC_TopBar1.Dock = DockStyle.Top;
+            uC_TopBar1.Location = new Point(0, 0);
+            uC_TopBar1.Name = "uC_TopBar1";
+            uC_TopBar1.Size = new Size(1054, 56);
+            uC_TopBar1.TabIndex = 0;
+            // 
+            // guna2PictureBox1
+            // 
+            guna2PictureBox1.BackColor = Color.FromArgb(255, 254, 234);
+            guna2PictureBox1.CustomizableEdges = customizableEdges1;
+            guna2PictureBox1.Image = Properties.Resources.picture_changepassword;
+            guna2PictureBox1.ImageRotate = 0F;
+            guna2PictureBox1.Location = new Point(439, 45);
+            guna2PictureBox1.Name = "guna2PictureBox1";
+            guna2PictureBox1.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            guna2PictureBox1.Size = new Size(615, 617);
+            guna2PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            guna2PictureBox1.TabIndex = 4;
+            guna2PictureBox1.TabStop = false;
+            // 
+            // guna2Elipse1
+            // 
+            guna2Elipse1.BorderRadius = 25;
+            guna2Elipse1.TargetControl = this;
+            // 
+            // guna2CustomGradientPanel1
+            // 
+            guna2CustomGradientPanel1.AutoSize = true;
+            guna2CustomGradientPanel1.BackColor = Color.White;
+            guna2CustomGradientPanel1.Controls.Add(linkLabel1);
+            guna2CustomGradientPanel1.Controls.Add(pictureBox1);
+            guna2CustomGradientPanel1.Controls.Add(guna2TextBox1);
+            guna2CustomGradientPanel1.Controls.Add(guna2TextBox2);
+            guna2CustomGradientPanel1.Controls.Add(guna2Button1);
+            guna2CustomGradientPanel1.Controls.Add(label3);
+            guna2CustomGradientPanel1.Controls.Add(label2);
+            guna2CustomGradientPanel1.Controls.Add(label1);
+            guna2CustomGradientPanel1.Controls.Add(guna2Panel1);
+            guna2CustomGradientPanel1.Cursor = Cursors.Hand;
+            guna2CustomGradientPanel1.CustomizableEdges = customizableEdges11;
+            guna2CustomGradientPanel1.FillColor = Color.FromArgb(255, 254, 234);
+            guna2CustomGradientPanel1.FillColor2 = Color.FromArgb(255, 254, 234);
+            guna2CustomGradientPanel1.FillColor3 = Color.FromArgb(255, 254, 234);
+            guna2CustomGradientPanel1.FillColor4 = Color.FromArgb(255, 254, 234);
+            guna2CustomGradientPanel1.Location = new Point(0, 45);
+            guna2CustomGradientPanel1.Name = "guna2CustomGradientPanel1";
+            guna2CustomGradientPanel1.ShadowDecoration.CustomizableEdges = customizableEdges12;
+            guna2CustomGradientPanel1.Size = new Size(536, 617);
+            guna2CustomGradientPanel1.TabIndex = 5;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Font = new Font("Ubuntu", 7F, FontStyle.Regular, GraphicsUnit.Point);
+            linkLabel1.LinkColor = SystemColors.ControlDarkDark;
+            linkLabel1.Location = new Point(161, 546);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(80, 16);
+            linkLabel1.TabIndex = 5;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Back to login";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.FromArgb(255, 254, 234);
+            pictureBox1.Cursor = Cursors.Hand;
+            pictureBox1.Image = Properties.Resources.logo_park_ease_oranye;
+            pictureBox1.Location = new Point(178, 63);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Padding = new Padding(10, 11, 10, 11);
+            pictureBox1.Size = new Size(181, 50);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // guna2TextBox1
+            // 
+            guna2TextBox1.BackColor = Color.Transparent;
+            guna2TextBox1.BorderColor = Color.FromArgb(255, 244, 133);
+            guna2TextBox1.BorderRadius = 15;
+            guna2TextBox1.BorderThickness = 3;
+            guna2TextBox1.CustomizableEdges = customizableEdges3;
+            guna2TextBox1.DefaultText = "";
+            guna2TextBox1.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            guna2TextBox1.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            guna2TextBox1.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            guna2TextBox1.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            guna2TextBox1.FillColor = Color.FromArgb(255, 249, 197);
+            guna2TextBox1.FocusedState.BorderColor = Color.Transparent;
+            guna2TextBox1.FocusedState.ForeColor = Color.FromArgb(226, 139, 0);
+            guna2TextBox1.Font = new Font("Ubuntu", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            guna2TextBox1.ForeColor = Color.FromArgb(226, 139, 0);
+            guna2TextBox1.HoverState.BorderColor = Color.Transparent;
+            guna2TextBox1.IconLeft = Properties.Resources.icon_key;
+            guna2TextBox1.IconLeftOffset = new Point(10, 0);
+            guna2TextBox1.IconRightOffset = new Point(10, 0);
+            guna2TextBox1.Location = new Point(99, 303);
+            guna2TextBox1.Margin = new Padding(3, 5, 3, 5);
+            guna2TextBox1.Name = "guna2TextBox1";
+            guna2TextBox1.PasswordChar = '\0';
+            guna2TextBox1.PlaceholderForeColor = Color.FromArgb(226, 139, 0);
+            guna2TextBox1.PlaceholderText = "Password";
+            guna2TextBox1.SelectedText = "";
+            guna2TextBox1.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            guna2TextBox1.Size = new Size(339, 48);
+            guna2TextBox1.TabIndex = 4;
+            guna2TextBox1.TextOffset = new Point(20, 0);
+            // 
+            // guna2TextBox2
+            // 
+            guna2TextBox2.BackColor = Color.Transparent;
+            guna2TextBox2.BorderColor = Color.FromArgb(255, 244, 133);
+            guna2TextBox2.BorderRadius = 15;
+            guna2TextBox2.BorderThickness = 3;
+            guna2TextBox2.CustomizableEdges = customizableEdges5;
+            guna2TextBox2.DefaultText = "";
+            guna2TextBox2.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            guna2TextBox2.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            guna2TextBox2.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            guna2TextBox2.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            guna2TextBox2.FillColor = Color.FromArgb(255, 249, 197);
+            guna2TextBox2.FocusedState.BorderColor = Color.Transparent;
+            guna2TextBox2.FocusedState.ForeColor = Color.FromArgb(226, 139, 0);
+            guna2TextBox2.Font = new Font("Ubuntu", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            guna2TextBox2.ForeColor = Color.FromArgb(226, 139, 0);
+            guna2TextBox2.HoverState.BorderColor = Color.Transparent;
+            guna2TextBox2.IconLeft = Properties.Resources.icon_key;
+            guna2TextBox2.IconLeftOffset = new Point(10, 0);
+            guna2TextBox2.IconRight = Properties.Resources.icon_eye;
+            guna2TextBox2.IconRightOffset = new Point(10, 0);
+            guna2TextBox2.Location = new Point(99, 380);
+            guna2TextBox2.Margin = new Padding(3, 5, 3, 5);
+            guna2TextBox2.Name = "guna2TextBox2";
+            guna2TextBox2.PasswordChar = '\0';
+            guna2TextBox2.PlaceholderForeColor = Color.FromArgb(226, 139, 0);
+            guna2TextBox2.PlaceholderText = "Password Confirmation";
+            guna2TextBox2.SelectedText = "";
+            guna2TextBox2.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            guna2TextBox2.Size = new Size(339, 48);
+            guna2TextBox2.TabIndex = 4;
+            guna2TextBox2.TextOffset = new Point(20, 0);
+            // 
+            // guna2Button1
+            // 
+            guna2Button1.CustomizableEdges = customizableEdges7;
+            guna2Button1.DisabledState.BorderColor = Color.DarkGray;
+            guna2Button1.DisabledState.CustomBorderColor = Color.DarkGray;
+            guna2Button1.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            guna2Button1.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            guna2Button1.FillColor = Color.FromArgb(226, 139, 0);
+            guna2Button1.Font = new Font("Ubuntu", 7.79999971F, FontStyle.Bold, GraphicsUnit.Point);
+            guna2Button1.ForeColor = Color.White;
+            guna2Button1.Location = new Point(99, 488);
+            guna2Button1.Name = "guna2Button1";
+            guna2Button1.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            guna2Button1.Size = new Size(339, 45);
+            guna2Button1.TabIndex = 3;
+            guna2Button1.Text = "Change Password";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.FromArgb(255, 254, 234);
+            label3.Font = new Font("Ubuntu", 7F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.ForeColor = SystemColors.ControlDarkDark;
+            label3.Location = new Point(99, 546);
+            label3.Name = "label3";
+            label3.Size = new Size(62, 16);
+            label3.TabIndex = 2;
+            label3.Text = "Not sure?";
+            label3.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.FromArgb(255, 254, 234);
+            label2.Font = new Font("Ubuntu", 7F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.ForeColor = SystemColors.ControlDarkDark;
+            label2.Location = new Point(87, 222);
+            label2.Name = "label2";
+            label2.Size = new Size(362, 32);
+            label2.TabIndex = 2;
+            label2.Text = "Revamp your account's security effortlessly and regain control \r\nwith our user-friendly and secure password change feature.";
+            label2.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Ubuntu", 23F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.FromArgb(64, 64, 64);
+            label1.Location = new Point(62, 161);
+            label1.Name = "label1";
+            label1.Size = new Size(412, 50);
+            label1.TabIndex = 1;
+            label1.Text = "CHANGE PASSWORD";
+            // 
+            // guna2Panel1
+            // 
+            guna2Panel1.AutoSize = true;
+            guna2Panel1.BackColor = Color.FromArgb(226, 139, 0);
+            guna2Panel1.CustomizableEdges = customizableEdges9;
+            guna2Panel1.Dock = DockStyle.Top;
+            guna2Panel1.Location = new Point(0, 0);
+            guna2Panel1.Name = "guna2Panel1";
+            guna2Panel1.ShadowDecoration.CustomizableEdges = customizableEdges10;
+            guna2Panel1.Size = new Size(536, 0);
+            guna2Panel1.TabIndex = 0;
+            // 
+            // guna2Elipse2
+            // 
+            guna2Elipse2.BorderRadius = 20;
+            guna2Elipse2.TargetControl = guna2Button1;
+            // 
+            // ChangePassword
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1054, 661);
+            Controls.Add(guna2CustomGradientPanel1);
+            Controls.Add(guna2PictureBox1);
+            Controls.Add(uC_TopBar1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "ChangePassword";
             Text = "Change_Password";
+            ((System.ComponentModel.ISupportInitialize)guna2PictureBox1).EndInit();
+            guna2CustomGradientPanel1.ResumeLayout(false);
+            guna2CustomGradientPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private UserControls.UC_TopBar uC_TopBar1;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
+        private Guna.UI2.WinForms.Guna2CustomGradientPanel guna2CustomGradientPanel1;
+        private PictureBox pictureBox1;
+        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox2;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Label label2;
+        private Label label1;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox1;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse2;
+        private Label label3;
+        private LinkLabel linkLabel1;
     }
 }
