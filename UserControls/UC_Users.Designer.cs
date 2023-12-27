a60a86fdd323ef6e3888cfd5b91ab5f3a70609e3
damara <damarmuhamad13@gmail.com>
2023-12-27 15:20:47 +0700

    feat: add table users page for dashboard

--- UserControls/UC_Users.Designer.cs
+++ UserControls/UC_Users.Designer.cs
@@ -29,29 +29,29 @@
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            DataGridViewCellStyle dataGridViewCellStyle4 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle5 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle6 = new DataGridViewCellStyle();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             tableLayoutPanel1 = new TableLayoutPanel();
-            guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
             guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
-            lblDesUsers = new Label();
-            lblUsers = new Label();
             tableUsers = new Guna.UI2.WinForms.Guna2DataGridView();
             Column1 = new DataGridViewTextBoxColumn();
             Column2 = new DataGridViewTextBoxColumn();
             Column3 = new DataGridViewTextBoxColumn();
             Column4 = new DataGridViewTextBoxColumn();
             Column5 = new DataGridViewTextBoxColumn();
+            guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            lblDesUsers = new Label();
+            lblUsers = new Label();
             guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(components);
             tableLayoutPanel1.SuspendLayout();
-            guna2Panel1.SuspendLayout();
             guna2Panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)tableUsers).BeginInit();
+            guna2Panel1.SuspendLayout();
             SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -69,80 +69,42 @@
             tableLayoutPanel1.Size = new Size(831, 664);
             tableLayoutPanel1.TabIndex = 0;
             // 
-            // guna2Panel1
-            // 
-            guna2Panel1.Controls.Add(lblDesUsers);
-            guna2Panel1.Controls.Add(lblUsers);
-            guna2Panel1.CustomizableEdges = customizableEdges5;
-            guna2Panel1.Dock = DockStyle.Fill;
-            guna2Panel1.Location = new Point(3, 3);
-            guna2Panel1.Name = "guna2Panel1";
-            guna2Panel1.ShadowDecoration.CustomizableEdges = customizableEdges6;
-            guna2Panel1.Size = new Size(825, 210);
-            guna2Panel1.TabIndex = 0;
-            // 
             // guna2Panel2
             // 
+            guna2Panel2.BackColor = Color.White;
             guna2Panel2.Controls.Add(tableUsers);
-            guna2Panel2.CustomizableEdges = customizableEdges7;
+            guna2Panel2.CustomizableEdges = customizableEdges1;
             guna2Panel2.Dock = DockStyle.Fill;
             guna2Panel2.Location = new Point(3, 219);
             guna2Panel2.Name = "guna2Panel2";
-            guna2Panel2.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            guna2Panel2.ShadowDecoration.CustomizableEdges = customizableEdges2;
             guna2Panel2.Size = new Size(825, 442);
             guna2Panel2.TabIndex = 0;
             // 
-            // lblDesUsers
-            // 
-            lblDesUsers.Anchor = AnchorStyles.None;
-            lblDesUsers.AutoSize = true;
-            lblDesUsers.BackColor = Color.FromArgb(255, 254, 234);
-            lblDesUsers.Font = new Font("Ubuntu", 7F, FontStyle.Regular, GraphicsUnit.Point);
-            lblDesUsers.ForeColor = SystemColors.ControlDarkDark;
-            lblDesUsers.Location = new Point(27, 124);
-            lblDesUsers.Name = "lblDesUsers";
-            lblDesUsers.Size = new Size(377, 32);
-            lblDesUsers.TabIndex = 8;
-            lblDesUsers.Text = "Welcome to Park Ease, an application that makes it easier for you \r\nto manage parking safely and comfortably.";
-            lblDesUsers.TextAlign = ContentAlignment.MiddleLeft;
-            // 
-            // lblUsers
-            // 
-            lblUsers.Anchor = AnchorStyles.None;
-            lblUsers.AutoSize = true;
-            lblUsers.BackColor = Color.Transparent;
-            lblUsers.Font = new Font("Ubuntu", 13F, FontStyle.Bold, GraphicsUnit.Point);
-            lblUsers.ForeColor = Color.FromArgb(64, 64, 64);
-            lblUsers.Location = new Point(27, 61);
-            lblUsers.Name = "lblUsers";
-            lblUsers.Size = new Size(153, 27);
-            lblUsers.TabIndex = 7;
-            lblUsers.Text = "TABLE USERS";
-            lblUsers.TextAlign = ContentAlignment.MiddleLeft;
-            // 
             // tableUsers
             // 
-            dataGridViewCellStyle4.BackColor = Color.White;
-            tableUsers.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle4;
+            dataGridViewCellStyle1.BackColor = Color.White;
+            tableUsers.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
             tableUsers.BackgroundColor = Color.FromArgb(255, 254, 234);
-            dataGridViewCellStyle5.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle5.BackColor = Color.FromArgb(226, 139, 0);
-            dataGridViewCellStyle5.Font = new Font("Ubuntu", 8.999999F, FontStyle.Bold, GraphicsUnit.Point);
-            dataGridViewCellStyle5.ForeColor = Color.White;
-            dataGridViewCellStyle5.SelectionBackColor = SystemColors.Highlight;
-            dataGridViewCellStyle5.SelectionForeColor = SystemColors.HighlightText;
-            dataGridViewCellStyle5.WrapMode = DataGridViewTriState.True;
-            tableUsers.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
-            tableUsers.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = Color.FromArgb(226, 139, 0);
+            dataGridViewCellStyle2.Font = new Font("Ubuntu", 8.999999F, FontStyle.Bold, GraphicsUnit.Point);
+            dataGridViewCellStyle2.ForeColor = Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.True;
+            tableUsers.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            tableUsers.ColumnHeadersHeight = 22;
+            tableUsers.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
             tableUsers.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3, Column4, Column5 });
-            dataGridViewCellStyle6.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle6.BackColor = Color.FromArgb(255, 254, 234);
-            dataGridViewCellStyle6.Font = new Font("Ubuntu", 8.999999F, FontStyle.Bold, GraphicsUnit.Point);
-            dataGridViewCellStyle6.ForeColor = Color.FromArgb(71, 69, 94);
-            dataGridViewCellStyle6.SelectionBackColor = Color.FromArgb(231, 229, 255);
-            dataGridViewCellStyle6.SelectionForeColor = Color.FromArgb(71, 69, 94);
-            dataGridViewCellStyle6.WrapMode = DataGridViewTriState.False;
-            tableUsers.DefaultCellStyle = dataGridViewCellStyle6;
+            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = Color.FromArgb(255, 254, 234);
+            dataGridViewCellStyle3.Font = new Font("Ubuntu", 8.999999F, FontStyle.Bold, GraphicsUnit.Point);
+            dataGridViewCellStyle3.ForeColor = Color.FromArgb(71, 69, 94);
+            dataGridViewCellStyle3.SelectionBackColor = Color.FromArgb(231, 229, 255);
+            dataGridViewCellStyle3.SelectionForeColor = Color.FromArgb(71, 69, 94);
+            dataGridViewCellStyle3.WrapMode = DataGridViewTriState.False;
+            tableUsers.DefaultCellStyle = dataGridViewCellStyle3;
             tableUsers.GridColor = SystemColors.ControlDark;
             tableUsers.Location = new Point(27, 3);
             tableUsers.Name = "tableUsers";
@@ -162,7 +124,7 @@
             tableUsers.ThemeStyle.HeaderStyle.BorderStyle = DataGridViewHeaderBorderStyle.None;
             tableUsers.ThemeStyle.HeaderStyle.Font = new Font("Ubuntu", 8.999999F, FontStyle.Bold, GraphicsUnit.Point);
             tableUsers.ThemeStyle.HeaderStyle.ForeColor = Color.White;
-            tableUsers.ThemeStyle.HeaderStyle.HeaightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            tableUsers.ThemeStyle.HeaderStyle.HeaightSizeMode = DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
             tableUsers.ThemeStyle.HeaderStyle.Height = 22;
             tableUsers.ThemeStyle.ReadOnly = false;
             tableUsers.ThemeStyle.RowsStyle.BackColor = Color.FromArgb(255, 254, 234);
@@ -203,6 +165,47 @@
             Column5.MinimumWidth = 6;
             Column5.Name = "Column5";
             // 
+            // guna2Panel1
+            // 
+            guna2Panel1.BackColor = Color.White;
+            guna2Panel1.Controls.Add(lblDesUsers);
+            guna2Panel1.Controls.Add(lblUsers);
+            guna2Panel1.CustomizableEdges = customizableEdges3;
+            guna2Panel1.Dock = DockStyle.Fill;
+            guna2Panel1.Location = new Point(3, 3);
+            guna2Panel1.Name = "guna2Panel1";
+            guna2Panel1.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            guna2Panel1.Size = new Size(825, 210);
+            guna2Panel1.TabIndex = 0;
+            // 
+            // lblDesUsers
+            // 
+            lblDesUsers.Anchor = AnchorStyles.None;
+            lblDesUsers.AutoSize = true;
+            lblDesUsers.BackColor = Color.White;
+            lblDesUsers.Font = new Font("Ubuntu", 7F, FontStyle.Regular, GraphicsUnit.Point);
+            lblDesUsers.ForeColor = SystemColors.ControlDarkDark;
+            lblDesUsers.Location = new Point(27, 124);
+            lblDesUsers.Name = "lblDesUsers";
+            lblDesUsers.Size = new Size(377, 32);
+            lblDesUsers.TabIndex = 8;
+            lblDesUsers.Text = "Welcome to Park Ease, an application that makes it easier for you \r\nto manage parking safely and comfortably.";
+            lblDesUsers.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // lblUsers
+            // 
+            lblUsers.Anchor = AnchorStyles.None;
+            lblUsers.AutoSize = true;
+            lblUsers.BackColor = Color.Transparent;
+            lblUsers.Font = new Font("Ubuntu", 13F, FontStyle.Bold, GraphicsUnit.Point);
+            lblUsers.ForeColor = Color.FromArgb(64, 64, 64);
+            lblUsers.Location = new Point(27, 61);
+            lblUsers.Name = "lblUsers";
+            lblUsers.Size = new Size(153, 27);
+            lblUsers.TabIndex = 7;
+            lblUsers.Text = "TABLE USERS";
+            lblUsers.TextAlign = ContentAlignment.MiddleLeft;
+            // 
             // guna2Elipse1
             // 
             guna2Elipse1.BorderRadius = 8;
@@ -217,10 +220,10 @@
             Name = "UC_Users";
             Size = new Size(831, 664);
             tableLayoutPanel1.ResumeLayout(false);
-            guna2Panel1.ResumeLayout(false);
-            guna2Panel1.PerformLayout();
             guna2Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)tableUsers).EndInit();
+            guna2Panel1.ResumeLayout(false);
+            guna2Panel1.PerformLayout();
             ResumeLayout(false);
         }
 
