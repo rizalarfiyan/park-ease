25ae98f541e0ac5637e8aa54b121ec07815a45a4
damara <damarmuhamad13@gmail.com>
2023-12-28 03:19:01 +0700

    feat: add form locations page

---
+++ View/Locations.Designer.cs
@@ -0,0 +1,381 @@
+﻿namespace ParkEase.View
+{
+    partial class Locations
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges15 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges16 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges13 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges14 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(components);
+            uC_TopBar1 = new UserControls.UC_TopBar();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
+            chbExit = new Guna.UI2.WinForms.Guna2CustomCheckBox();
+            btnCencel = new Guna.UI2.WinForms.Guna2Button();
+            btnSave = new Guna.UI2.WinForms.Guna2Button();
+            label5 = new Label();
+            txtName = new Guna.UI2.WinForms.Guna2TextBox();
+            label4 = new Label();
+            txtID = new Guna.UI2.WinForms.Guna2TextBox();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            tableLayoutPanel1.SuspendLayout();
+            guna2Panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)guna2PictureBox1).BeginInit();
+            guna2Panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // guna2Elipse1
+            // 
+            guna2Elipse1.BorderRadius = 25;
+            guna2Elipse1.TargetControl = this;
+            // 
+            // uC_TopBar1
+            // 
+            uC_TopBar1.Dock = DockStyle.Top;
+            uC_TopBar1.Location = new Point(0, 0);
+            uC_TopBar1.Name = "uC_TopBar1";
+            uC_TopBar1.Size = new Size(1018, 56);
+            uC_TopBar1.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            tableLayoutPanel1.BackColor = Color.FromArgb(255, 254, 234);
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Controls.Add(guna2Panel1, 0, 0);
+            tableLayoutPanel1.Controls.Add(guna2Panel2, 1, 0);
+            tableLayoutPanel1.Location = new Point(0, 45);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Size = new Size(1018, 524);
+            tableLayoutPanel1.TabIndex = 1;
+            // 
+            // guna2Panel1
+            // 
+            guna2Panel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            guna2Panel1.Controls.Add(guna2PictureBox1);
+            guna2Panel1.CustomizableEdges = customizableEdges3;
+            guna2Panel1.Location = new Point(3, 3);
+            guna2Panel1.Name = "guna2Panel1";
+            guna2Panel1.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            guna2Panel1.Size = new Size(503, 518);
+            guna2Panel1.TabIndex = 0;
+            // 
+            // guna2PictureBox1
+            // 
+            guna2PictureBox1.Anchor = AnchorStyles.None;
+            guna2PictureBox1.BackColor = Color.FromArgb(255, 254, 234);
+            guna2PictureBox1.CustomizableEdges = customizableEdges1;
+            guna2PictureBox1.Image = Properties.Resources.picture_locations;
+            guna2PictureBox1.ImageRotate = 0F;
+            guna2PictureBox1.Location = new Point(-16, 1);
+            guna2PictureBox1.Name = "guna2PictureBox1";
+            guna2PictureBox1.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            guna2PictureBox1.Size = new Size(535, 517);
+            guna2PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            guna2PictureBox1.TabIndex = 7;
+            guna2PictureBox1.TabStop = false;
+            // 
+            // guna2Panel2
+            // 
+            guna2Panel2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            guna2Panel2.Controls.Add(chbExit);
+            guna2Panel2.Controls.Add(btnCencel);
+            guna2Panel2.Controls.Add(btnSave);
+            guna2Panel2.Controls.Add(label5);
+            guna2Panel2.Controls.Add(txtName);
+            guna2Panel2.Controls.Add(label4);
+            guna2Panel2.Controls.Add(txtID);
+            guna2Panel2.Controls.Add(label3);
+            guna2Panel2.Controls.Add(label2);
+            guna2Panel2.Controls.Add(label1);
+            guna2Panel2.CustomizableEdges = customizableEdges15;
+            guna2Panel2.Location = new Point(512, 3);
+            guna2Panel2.Name = "guna2Panel2";
+            guna2Panel2.ShadowDecoration.CustomizableEdges = customizableEdges16;
+            guna2Panel2.Size = new Size(503, 518);
+            guna2Panel2.TabIndex = 1;
+            // 
+            // chbExit
+            // 
+            chbExit.Anchor = AnchorStyles.None;
+            chbExit.CheckedState.BorderColor = Color.FromArgb(226, 139, 0);
+            chbExit.CheckedState.BorderRadius = 8;
+            chbExit.CheckedState.BorderThickness = 0;
+            chbExit.CheckedState.FillColor = Color.FromArgb(226, 139, 0);
+            chbExit.CustomizableEdges = customizableEdges5;
+            chbExit.Location = new Point(128, 339);
+            chbExit.Name = "chbExit";
+            chbExit.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            chbExit.Size = new Size(30, 30);
+            chbExit.TabIndex = 22;
+            chbExit.Text = "guna2CustomCheckBox1";
+            chbExit.UncheckedState.BorderColor = Color.FromArgb(255, 231, 70);
+            chbExit.UncheckedState.BorderRadius = 8;
+            chbExit.UncheckedState.BorderThickness = 0;
+            chbExit.UncheckedState.FillColor = Color.FromArgb(255, 231, 70);
+            // 
+            // btnCencel
+            // 
+            btnCencel.Anchor = AnchorStyles.None;
+            btnCencel.BorderRadius = 8;
+            btnCencel.Cursor = Cursors.Hand;
+            btnCencel.CustomizableEdges = customizableEdges7;
+            btnCencel.DisabledState.BorderColor = Color.DarkGray;
+            btnCencel.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnCencel.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnCencel.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnCencel.FillColor = Color.FromArgb(255, 231, 70);
+            btnCencel.Font = new Font("Ubuntu", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btnCencel.ForeColor = Color.White;
+            btnCencel.Location = new Point(128, 424);
+            btnCencel.Margin = new Padding(0);
+            btnCencel.Name = "btnCencel";
+            btnCencel.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            btnCencel.Size = new Size(154, 57);
+            btnCencel.TabIndex = 20;
+            btnCencel.Text = "Cancel";
+            btnCencel.TextOffset = new Point(0, -2);
+            // 
+            // btnSave
+            // 
+            btnSave.Anchor = AnchorStyles.None;
+            btnSave.BorderRadius = 8;
+            btnSave.Cursor = Cursors.Hand;
+            btnSave.CustomizableEdges = customizableEdges9;
+            btnSave.DisabledState.BorderColor = Color.DarkGray;
+            btnSave.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnSave.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnSave.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnSave.FillColor = Color.FromArgb(226, 139, 0);
+            btnSave.Font = new Font("Ubuntu", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btnSave.ForeColor = Color.White;
+            btnSave.Location = new Point(295, 424);
+            btnSave.Margin = new Padding(0);
+            btnSave.Name = "btnSave";
+            btnSave.ShadowDecoration.CustomizableEdges = customizableEdges10;
+            btnSave.Size = new Size(144, 57);
+            btnSave.TabIndex = 21;
+            btnSave.Text = "Save";
+            btnSave.TextOffset = new Point(0, -2);
+            // 
+            // label5
+            // 
+            label5.Anchor = AnchorStyles.None;
+            label5.AutoSize = true;
+            label5.BackColor = Color.FromArgb(255, 254, 234);
+            label5.Font = new Font("Ubuntu", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.ForeColor = Color.FromArgb(226, 139, 0);
+            label5.Location = new Point(44, 344);
+            label5.Name = "label5";
+            label5.Size = new Size(54, 21);
+            label5.TabIndex = 14;
+            label5.Text = "Exit ?";
+            label5.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // txtName
+            // 
+            txtName.Anchor = AnchorStyles.None;
+            txtName.BackColor = Color.Transparent;
+            txtName.BorderColor = Color.FromArgb(255, 244, 133);
+            txtName.BorderRadius = 15;
+            txtName.BorderThickness = 3;
+            txtName.CustomizableEdges = customizableEdges11;
+            txtName.DefaultText = "";
+            txtName.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtName.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtName.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtName.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtName.FillColor = Color.FromArgb(255, 249, 197);
+            txtName.FocusedState.BorderColor = Color.Transparent;
+            txtName.FocusedState.ForeColor = Color.FromArgb(226, 139, 0);
+            txtName.Font = new Font("Ubuntu", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            txtName.ForeColor = Color.FromArgb(226, 139, 0);
+            txtName.HoverState.BorderColor = Color.Transparent;
+            txtName.IconLeftOffset = new Point(10, 0);
+            txtName.IconRightOffset = new Point(10, 0);
+            txtName.Location = new Point(125, 256);
+            txtName.Margin = new Padding(4, 6, 4, 6);
+            txtName.Name = "txtName";
+            txtName.PasswordChar = '\0';
+            txtName.PlaceholderForeColor = Color.FromArgb(226, 139, 0);
+            txtName.PlaceholderText = " ";
+            txtName.SelectedText = "";
+            txtName.ShadowDecoration.CustomizableEdges = customizableEdges12;
+            txtName.Size = new Size(329, 45);
+            txtName.TabIndex = 18;
+            txtName.TextOffset = new Point(20, 0);
+            // 
+            // label4
+            // 
+            label4.Anchor = AnchorStyles.None;
+            label4.AutoSize = true;
+            label4.BackColor = Color.FromArgb(255, 254, 234);
+            label4.Font = new Font("Ubuntu", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.ForeColor = Color.FromArgb(226, 139, 0);
+            label4.Location = new Point(44, 268);
+            label4.Name = "label4";
+            label4.Size = new Size(58, 21);
+            label4.TabIndex = 15;
+            label4.Text = "Name";
+            label4.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // txtID
+            // 
+            txtID.Anchor = AnchorStyles.None;
+            txtID.BackColor = Color.Transparent;
+            txtID.BorderColor = Color.FromArgb(255, 244, 133);
+            txtID.BorderRadius = 15;
+            txtID.BorderThickness = 3;
+            txtID.CustomizableEdges = customizableEdges13;
+            txtID.DefaultText = "";
+            txtID.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtID.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtID.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtID.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtID.FillColor = Color.FromArgb(255, 249, 197);
+            txtID.FocusedState.BorderColor = Color.Transparent;
+            txtID.FocusedState.ForeColor = Color.FromArgb(226, 139, 0);
+            txtID.Font = new Font("Ubuntu", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            txtID.ForeColor = Color.FromArgb(226, 139, 0);
+            txtID.HoverState.BorderColor = Color.Transparent;
+            txtID.IconLeftOffset = new Point(10, 0);
+            txtID.IconRightOffset = new Point(10, 0);
+            txtID.Location = new Point(125, 180);
+            txtID.Margin = new Padding(4, 6, 4, 6);
+            txtID.Name = "txtID";
+            txtID.PasswordChar = '\0';
+            txtID.PlaceholderForeColor = Color.FromArgb(226, 139, 0);
+            txtID.PlaceholderText = " ";
+            txtID.SelectedText = "";
+            txtID.ShadowDecoration.CustomizableEdges = customizableEdges14;
+            txtID.Size = new Size(329, 45);
+            txtID.TabIndex = 19;
+            txtID.TextOffset = new Point(20, 0);
+            // 
+            // label3
+            // 
+            label3.Anchor = AnchorStyles.None;
+            label3.AutoSize = true;
+            label3.BackColor = Color.FromArgb(255, 254, 234);
+            label3.Font = new Font("Ubuntu", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.ForeColor = Color.FromArgb(226, 139, 0);
+            label3.Location = new Point(44, 192);
+            label3.Name = "label3";
+            label3.Size = new Size(28, 21);
+            label3.TabIndex = 16;
+            label3.Text = "ID";
+            label3.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.None;
+            label2.AutoSize = true;
+            label2.BackColor = Color.FromArgb(255, 254, 234);
+            label2.Font = new Font("Ubuntu", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.ForeColor = SystemColors.ControlDarkDark;
+            label2.Location = new Point(36, 105);
+            label2.Name = "label2";
+            label2.Size = new Size(428, 38);
+            label2.TabIndex = 10;
+            label2.Text = "This location will help you to determine a good parking place \r\naccording to your wishes. Safe and comfortable greetings.";
+            label2.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.None;
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Ubuntu", 23F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.FromArgb(64, 64, 64);
+            label1.Location = new Point(36, 38);
+            label1.Name = "label1";
+            label1.Size = new Size(243, 50);
+            label1.TabIndex = 9;
+            label1.Text = "LOCATIONS";
+            label1.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // Locations
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1018, 567);
+            Controls.Add(tableLayoutPanel1);
+            Controls.Add(uC_TopBar1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Locations";
+            Text = "Locations";
+            tableLayoutPanel1.ResumeLayout(false);
+            guna2Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)guna2PictureBox1).EndInit();
+            guna2Panel2.ResumeLayout(false);
+            guna2Panel2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
+        private UserControls.UC_TopBar uC_TopBar1;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel2;
+        private Label label2;
+        private Label label1;
+        private Guna.UI2.WinForms.Guna2Button btnCencel;
+        private Guna.UI2.WinForms.Guna2Button btnSave;
+        private Label label5;
+        private Guna.UI2.WinForms.Guna2TextBox txtName;
+        private Label label4;
+        private Guna.UI2.WinForms.Guna2TextBox txtID;
+        private Label label3;
+        private Guna.UI2.WinForms.Guna2CustomCheckBox chbExit;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+    }
+}
