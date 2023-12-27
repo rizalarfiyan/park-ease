bd0269ccbcedcaf438d27b9bf88c76e21e42a1a1
damara <damarmuhamad13@gmail.com>
2023-12-28 01:14:00 +0700

    fix: fixing style in table vehicle-type for dashboard page

--- Constants/App.cs
+++ Constants/App.cs
@@ -4,7 +4,7 @@
     {
         public const String APP_NAME = "Park Ease";
 
-        public const String API_BASE_URL = "http://localhost:8080";
+        public const String API_BASE_URL = "https://parkease.fied.tech/";
 
         public const int DEFAULT_PAGINATION_LIMIT = 10;
     }
