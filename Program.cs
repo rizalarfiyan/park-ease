0dabd99069c39e88e1baded2818dfa914ce3a61b
damara <damarmuhamad13@gmail.com>
2023-12-31 17:59:51 +0700

    feat: add dashboard active page

--- Program.cs
+++ Program.cs
@@ -21,7 +21,7 @@ namespace ParkEase
             ApplicationConfiguration.Initialize();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmDashboard());
+            Application.Run(new SplashScreen());
         }
     }
 }
